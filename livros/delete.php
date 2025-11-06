a96bcb0ce3972ba99efc1edc6d52350abb03a53b
polzomanga <victor_polzin@estudante.sesisenai.org.br>
2025-11-06 08:35:45 -0300

    add

--- livros/delete.php
+++ livros/delete.php
@@ -1,3 +1,23 @@
 <?php
-// Excluir livro
+require_once '../config/db.php';
+
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+
+// Verifica se existe
+$stmt = $conn->prepare('SELECT * FROM livros WHERE id_livro = ?');
+$stmt->bind_param('i', $id);
+$stmt->execute();
+$res = $stmt->get_result();
+if (!$res->fetch_assoc()) {
+    echo 'Livro não encontrado.';
+    exit;
+}
+
+// Exclui
+$stmt = $conn->prepare('DELETE FROM livros WHERE id_livro = ?');
+$stmt->bind_param('i', $id);
+$stmt->execute();
+
+header('Location: index.php');
+exit;
 ?>
